55f6190f1f3c00887c8e3fb8c00a90c56419a8e9
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-05-01 09:25:46 -0300

    Send files to repository

---
+++ src/middlewares/handleTokenMiddleware.js
@@ -0,0 +1,14 @@
+function handleTokenMiddleware(request, response, next) {
+    let bearerHeader = request.headers['authorization']
+
+    if (bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        request.token = bearerToken;
+        next();
+    } else {
+        return response.sendStatus(403);
+    }
+}
+
+module.exports =  handleTokenMiddleware ;
