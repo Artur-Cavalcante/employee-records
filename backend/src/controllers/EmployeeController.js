aec86d6b4d4e5b7d480c7914583d4f187c590696
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-05-13 22:41:30 -0300

    fix(backend): remove error in delete

--- backend/src/controllers/EmployeeController.js
+++ backend/src/controllers/EmployeeController.js
@@ -1,138 +1,201 @@
 const Employee = require('../models/Employee');
 const jwt = require('jsonwebtoken');
-const secretKey = '68dfe874e23514b88da48f07f0eb85e6';
+require("dotenv").config();
 
-async function index(request, response, next) {
-    let isAuthenticate = jwt.verify(request.token, secretKey, (error) =>{
-        if (error){
+const secretKey = "68dfe874e23514b88da48f07f0eb85e6";
+
+async function index(request, response) {
+    let isAuthenticate = jwt.verify(request.token, secretKey, (error) => {
+        if (error) {
             console.log('Error VerifyToken ->', error);
             //Verfiy 'JsonWebTokenError'
             //Verify 'TokenExpiredError'
             return false;
-        }else{
+        } else {
             return true;
         }
     })
 
-    if(isAuthenticate === true){
-            try {
-                let Employees = await Employee.find();
-                return response.json(Employees);
-            } catch (err) {
-                console.log('Error server ->',err)
-        
-            }
-    }else{
+    if (isAuthenticate === true) {
+        try {
+            let Employees = await Employee.find();
+            return response.json(Employees);
+        } catch (err) {
+            console.log("Error server ->", err);
+
+        }
+    } else {
         return response.sendStatus(403);
     }
 };
 
-async function show(request, response, next) {
+async function show(request, response) {
 
-
-    const { cpf } = request.params;
-
-    try {
-        
-        let Employee_unique = await Employee.findOne({ cpf });
-
-        //Case not found
-        if (Employee_unique === null) {
-            Employee_unique = { "Aviso": "Funcionário não Encontrado!" };
-            return response.json(Employee_unique);
+    let isAuthenticate = jwt.verify(request.token, secretKey, (error) => {
+        if (error) {
+            console.log('Error VerifyToken ->', error);
+            //Verfiy 'JsonWebTokenError'
+            //Verify 'TokenExpiredError'
+            return false;
+        } else {
+            return true;
         }
-        
-        return response.json(Employee_unique);
-    
-    } catch (err) {
-        console.log('Server error ->  ', err)
+    })
 
+    if (isAuthenticate === true) {
+        const { cpf } = request.params;
+
+        try {
+
+            let Employee_unique = await Employee.findOne({ cpf });
+
+            //Case not found
+            if (Employee_unique === null) {
+                Employee_unique = { "Aviso": "Funcionário não Encontrado!" };
+                return response.json(Employee_unique);
+            }
+
+            return response.json(Employee_unique);
+
+        } catch (err) {
+            console.log('Server error ->  ', err)
+
+        }
+    } else {
+        return response.sendStatus(403);
     }
+
 };
 
 async function store(request, response) {
-    const {
-        name_employee,
-        number_phone_employee,
-        birthday_date_employee,
-        cpf,
-        contract_date_employee,
-        address_employee,
-        occupation_employee,
-        salary_employee,
-        sector_employee,
-        employee_is_active
-    } = request.body;
-    
-    try {
-
-        let new_employee = await Employee.findOne({ cpf })
-
-        if (!new_employee) {
-            new_employee = await Employee.create({
-                name_employee,
-                number_phone_employee,
-                birthday_date_employee,
-                cpf,
-                contract_date_employee,
-                address_employee,
-                occupation_employee,
-                salary_employee,
-                sector_employee,
-                employee_is_active
-            });
+    let isAuthenticate = jwt.verify(request.token, secretKey, (error) => {
+        if (error) {
+            console.log('Error VerifyToken ->', error);
+            //Verfiy 'JsonWebTokenError'
+            //Verify 'TokenExpiredError'
+            return false;
         } else {
-            new_employee = { "Aviso": "Usuário já cadastrado" };
-        };
-        
-        return response.json(new_employee);
-    
-    } catch (err) {
-        console.log('Server error ->  ', err)
+            return true;
+        }
+    })
+
+    if (isAuthenticate === true) {
+        const {
+            name_employee,
+            number_phone_employee,
+            birthday_date_employee,
+            cpf,
+            contract_date_employee,
+            address_employee,
+            occupation_employee,
+            salary_employee,
+            sector_employee,
+            employee_is_active
+        } = request.body;
+
+        try {
+
+            let new_employee = await Employee.findOne({ cpf })
+
+            if (!new_employee) {
+                new_employee = await Employee.create({
+                    name_employee,
+                    number_phone_employee,
+                    birthday_date_employee,
+                    cpf,
+                    contract_date_employee,
+                    address_employee,
+                    occupation_employee,
+                    salary_employee,
+                    sector_employee,
+                    employee_is_active
+                });
+            } else {
+                new_employee = { "Aviso": "Usuário já cadastrado" };
+            };
+
+            return response.json(new_employee);
+
+        } catch (err) {
+            console.log('Server error ->  ', err)
+        }
+    } else {
+        return response.sendStatus(403);
     }
 
 };
 
-async function update(request, response, next) {
-    //Is obrigatorie the cpf, and other params will to be add
-    const { cpf, ...updates } = request.body;
-
-    try {
-
-        let update_employee = await Employee.findOne({ cpf });
-
-        if (update_employee) {
-            const { name_employee } = await Employee.findOneAndUpdate({ cpf }, updates);
-            update_employee = { "Funcionário Alterado": name_employee };
+async function update(request, response) {
+    let isAuthenticate = jwt.verify(request.token, secretKey, (error) => {
+        if (error) {
+            console.log('Error VerifyToken ->', error);
+            //Verfiy 'JsonWebTokenError'
+            //Verify 'TokenExpiredError'
+            return false;
         } else {
-            update_employee = { "Aviso": "Funcionário não encontrado!" };
-        };
-        return response.json(update_employee);
-    } catch (err) {
-        console.log('Server error ->  ', err)
+            return true;
+        }
+    })
 
-    };
+    if (isAuthenticate === true) {
+        //Is obrigatorie the cpf, and other params will to be add
+        const { cpf, ...updates } = request.body;
+
+        try {
+
+            let update_employee = await Employee.findOne({ cpf });
+
+            if (update_employee) {
+                const { name_employee } = await Employee.findOneAndUpdate({ cpf }, updates);
+                update_employee = { "Funcionário Alterado": name_employee };
+            } else {
+                update_employee = { "Aviso": "Funcionário não encontrado!" };
+            };
+            return response.json(update_employee);
+        } catch (err) {
+            console.log('Server error ->  ', err)
+
+        };
+    } else {
+        return response.sendStatus(403);
+    }
 
 };
 
-async function destroy(request, response, next) {
-    const { cpf } = request.body;
-
-    try {
-        let delete_employee = await Employee.findOne({ cpf });
-
-        if (delete_employee) {
-            delete_employee = await Employee.findOneAndDelete({ cpf });
+async function destroy(request, response) {
+    let isAuthenticate = jwt.verify(request.token, secretKey, (error) => {
+        if (error) {
+            console.log('Error VerifyToken ->', error);
+            //Verfiy 'JsonWebTokenError'
+            //Verify 'TokenExpiredError'
+            return false;
         } else {
-            delete_employee = { "Aviso": "Funcionário inexistente!" }
+            return true;
         }
+    })
 
-        return response.json(delete_employee);
+    if (isAuthenticate === true) {
+        const { cpf } = request.body;
+        console.log("Cpf", request.body);
 
-    } catch (err) {
-        console.log('Server error ->  ', err)
+        try {
+            let delete_employee = await Employee.findOne({ cpf });
 
-    };
+            if (delete_employee) {
+                delete_employee = await Employee.findOneAndDelete({ cpf });
+            } else {
+                delete_employee = { "Aviso": "Funcionário inexistente!" }
+            }
+
+            return response.json(delete_employee);
+
+        } catch (err) {
+            console.log('Server error ->  ', err)
+
+        };
+    } else {
+        return response.sendStatus(403);
+    }
 };
 
 
