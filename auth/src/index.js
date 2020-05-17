4788e33c80626a991287b910af9c50d22901db2f
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-05-17 19:15:36 -0300

    fix(auth): use process.env.port

--- auth/src/index.js
+++ auth/src/index.js
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-const PORT = process.env.PORT
+const PORT = `${process.env.PORT}` || 3000;
 app.listen(PORT, () =>{console.log('Server running at port: ' + PORT)});
