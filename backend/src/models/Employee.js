b7c3642fb5c90ae202d93e3e6b261c95a84bc702
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-06-06 11:28:32 -0300

    fix(backend): alter all type to string

--- backend/src/models/Employee.js
+++ backend/src/models/Employee.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
     name_employee: String,
-    number_phone_employee: Number,
-    birthday_date_employee: Date,
+    number_phone_employee: String,
+    birthday_date_employee: String,
     cpf: {
         type: String,
         required: true
     },
-    contract_date_employee: Date,
+    contract_date_employee: String,
     address_employee: String,
     occupation_employee: String,
-    salary_employee: Number,
+    salary_employee: String,
     sector_employee: String,
-    employee_is_active: Boolean,
+    employee_is_active: String,
 },
 { collection: 'employee-records' }
 );
