de6858ba95d8c356671c9a23781768e3d0ef96de
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-06-07 00:20:30 -0300

    test 4

--- src/middlewares/handleTokenMiddleware.js
+++ src/middlewares/handleTokenMiddleware.js
@@ -1,6 +1,6 @@
 function handleTokenMiddleware(request, response, next) {
     let bearerHeader = request.headers['authorization']
-    console.log("Request ->", request.headers['authorization'].split(' '));
+    console.log("Request ->", request.headers['authorization'].split(' ')[1]);
     if (bearerHeader) {
         const bearer = bearerHeader.split(' ');
         const bearerToken = bearer[1];
