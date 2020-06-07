b6cbf001a50968cf669c47979f98b46431f81cd8
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-06-07 00:41:26 -0300

    teste

--- mobile/src/utils/masks/maskCpf.js
+++
@@ -1,7 +0,0 @@
-function maskCpf(value) {
-    //Remove if not number
-    return value
-        .replace(/\D/g, '');
-};
-
-module.exports = maskCpf;
--- mobile/src/utils/masks/maskInputDate.js
+++
@@ -1,8 +0,0 @@
-function maskInputDate(value){
-    return value
-            .replace(/\D/g, '')//If not a number, it is remove
-            .replace(/(\d{4})(\d)/, '$1/$2')
-            .replace(/(\d{2})(\d)/, '$1/$2')
-};
-
-module.exports = maskInputDate ;
